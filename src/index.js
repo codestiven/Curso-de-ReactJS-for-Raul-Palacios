2b7f7b499b21ae56a0d3ad81611f82d85144c32f
Stiven <codestiven@gmail.com>
2024-01-04 15:15:56 -0400

    first commit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+// const element = document.createElement('h1');
+// element.innerText = 'Hello, world!';
+// const container = document.getElementById('root');
+// container.appendChild(element);
+
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const user = {
+  firstName: "Johen",
+  lastName: "Doe",
+  age: 50,
+  avatarUrl: "https://randomuser.me/api/portraits/men/45.jpg",
+};
+
+const nombre = "Stiven";	
+const element = <img src={user.avatarUrl}/>; ;
+const container = document.getElementById("root");
+
+ReactDOM.render(element, container)
